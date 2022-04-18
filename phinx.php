411feb54963845763670b2cb0d297387eea8a6d4
michelemeiira <102440706+michelemeiira@users.noreply.github.com>
2022-04-18 19:20:25 -0300

    Adding ENV variables on Production and Testing environments

---
+++ phinx.php
@@ -0,0 +1,44 @@
+<?php
+
+$env = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$env->load();
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_DATABASE'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => $_ENV['DB_CHARSET'],
+        ],
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_DATABASE'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => $_ENV['DB_CHARSET'],
+        ],
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_DATABASE'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => $_ENV['DB_CHARSET'],
+        ]
+    ],
+    'version_order' => 'creation'
+];
